4c25dd81f7ec333a106ca0f7e24eddb19e6d6b72
Valerio Di Zio <diziovalerio@gmail.com>
2024-10-17 17:03:38 +0200

    fix: shown both values of sensitive fields in the metrics printout

--- fairlib/metrics/_utils.py
+++ fairlib/metrics/_utils.py
@@ -1,14 +1,14 @@
-from pandas.api.types import is_numeric_dtype
 import warnings
 import numpy as np
 
 
-def _get_values(values: np.ndarray) -> tuple[np.ndarray, int]:
+def _get_values(values: np.ndarray, is_target=False) -> tuple[np.ndarray, int]:
     values = np.unique(values)
     values.sort()
     length = len(values)
     if len(values) == 2:
-        length -= 1
+        if is_target:
+            length -= 1
         if values[0] == 0:
             values = values[::-1]
     return values, length
@@ -17,7 +17,7 @@ def _get_values(values: np.ndarray) -> tuple[np.ndarray, int]:
 def check_and_setup(as_dict, sensitive_column, target_column):
     if len(target_column) != len(sensitive_column):
         raise ValueError("Target and sensitive columns must have the same length")
-    target_values, target_len = _get_values(target_column)
+    target_values, target_len = _get_values(target_column, is_target=True)
     sensitive_values, sensitive_len = _get_values(sensitive_column)
     if sensitive_len < 2:
         warnings.warn(
