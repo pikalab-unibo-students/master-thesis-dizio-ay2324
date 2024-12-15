969c4510149e75e9f8385485731b6e49a2158b20
Valerio Di Zio <diziovalerio@gmail.com>
2024-12-15 15:54:46 +0100

    fix: remove clip from keras metrics

--- fairlib/inprocessing/_keras_metrics.py
+++ fairlib/inprocessing/_keras_metrics.py
@@ -15,12 +15,6 @@ def _statistical_parity_difference_loss(y_true, y_pred, sensitive_attr):
     Returns:
     A measure of statistical parity difference as a loss.
     """
-    # Ensure that y_pred is limited between 0 and 1
-    # epsilon: used to prevent division by 0
-    y_pred = keras_ops.clip(
-        y_pred, keras_backend.epsilon(), 1 - keras_backend.epsilon()
-    )
-
     #  Groups based on the sensitive attribute
     group_positive = keras_ops.cast(
         keras_ops.equal(sensitive_attr, 1), keras_backend.floatx()
@@ -55,11 +49,6 @@ def _disparate_impact_loss(y_true, y_pred, sensitive_attr):
     Returns:
     A measure of disparate impact as a loss.
     """
-    # Ensure that y_pred is limited between 0 and 1
-    y_pred = keras_ops.clip(
-        y_pred, keras_backend.epsilon(), 1 - keras_backend.epsilon()
-    )
-
     # Groups based on the sensitive attribute
     group_positive = keras_ops.cast(
         keras_ops.equal(sensitive_attr, 1), keras_backend.floatx()
--- tests/inprocessing/test_fauci.py
+++ tests/inprocessing/test_fauci.py
@@ -1,51 +1,70 @@
 import unittest
 import openml
-
 import fairlib as fl
 from fairlib import keras
 from fairlib.inprocessing import Fauci
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Input
 
 
-def create_model():
-    model = Sequential()
-    model.add(Dense(32, activation="relu"))
-    model.add(Dense(16, activation="relu"))
-    model.add(Dense(8, activation="relu"))
-    model.add(Dense(1, activation="sigmoid"))
-    return model
+# Function to create a basic neural network model
+def create_model(input_shape):
+    return Sequential([
+        Input((input_shape,)),
+        Dense(64, activation="relu"),
+        Dense(32, activation="relu"),
+        Dense(16, activation="relu"),
+        Dense(8, activation="relu"),
+        Dense(1, activation="sigmoid")
+    ])
 
 
+# Function to evaluate a model's accuracy and fairness metric
 def evaluate_model(model, X_test, y_test, typeOfEval):
     predictions = model.predict(X_test.astype(float))
     y_pred = (predictions > 0.5).astype(int)
     accuracy = accuracy_score(y_test, y_pred)
+
+    # Convert test set to FairLib DataFrame
     X_test["income"] = y_pred
     dataset = fl.DataFrame(X_test)
     dataset.targets = "income"
     dataset.sensitive = "sex"
+
+    # Evaluate fairness metric
     if typeOfEval == "spd":
         metric = dataset.statistical_parity_difference()
     elif typeOfEval == "di":
         metric = dataset.disparate_impact()
     else:
-        raise "Metric Not Found"
+        raise ValueError("Metric Not Found")
     return accuracy, metric
 
 
+# Test class for Fauci fairness-aware learning model
 class TestFauci(unittest.TestCase):
 
+    EPOCHS = 10
+    BATCH_SIZE = 100
+    VALIDATION_SPLIT = 0.33
+
     def setUp(self):
+        # Set random seed for reproducibility
         keras.utils.set_random_seed(420)
+
+        # Load dataset from OpenML
         dataset = openml.datasets.get_dataset(179)
         X, y, _, names = dataset.get_data(target=dataset.default_target_attribute)
+
+        # Impute missing values
         imputer = SimpleImputer(strategy="most_frequent")
         X_imputed = imputer.fit_transform(X)
+
+        # Encode categorical features
         X_discretized = X_imputed.copy()
         for col in X.columns:
             if X[col].dtype == "category":
@@ -53,118 +72,94 @@ class TestFauci(unittest.TestCase):
                 X_discretized[:, X.columns.get_loc(col)] = le.fit_transform(
                     X_discretized[:, X.columns.get_loc(col)]
                 )
+
         self.X = fl.DataFrame(X_discretized, columns=names)
+        self.num_features = self.X.shape[1]
         self.y = y.apply(lambda x: x == ">50K").astype(int)
-        self.EPOCHS = 20
+
+    def train_and_evaluate(self, regularizer, fairness_metric):
+        # Prepare data for Fauci
+        X_train = self.X.copy()
+        y_train = self.y.copy()
+        X_train["income"] = y_train
+        fauci_train_dataset = fl.DataFrame(X_train)
+        fauci_train_dataset.targets = "income"
+        fauci_train_dataset.sensitive = "sex"
+        X_train.drop(columns=["income"], inplace=True)
+
+        # Train Fauci model
+        fauci_model = create_model(self.num_features)
+        fauciModel = Fauci(
+            fauci_model,
+            loss="binary_crossentropy",
+            regularizer=regularizer,
+            optimizer="adam",
+            metrics=["accuracy"]
+        )
+        fauciModel.fit(
+            fauci_train_dataset,
+            converting_to_type=float,
+            epochs=self.EPOCHS,
+            batch_size=self.BATCH_SIZE,
+            validation_split=self.VALIDATION_SPLIT
+        )
+
+        # Evaluate Fauci model
+        fauci_accuracy, fauci_metric = evaluate_model(
+            fauciModel, X_train, y_train, fairness_metric
+        )
+
+        # Train default model
+        default_model = create_model(self.num_features)
+        default_model.compile(
+            loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
+        )
+        default_model.fit(
+            X_train.astype(float),
+            y_train.astype(float),
+            epochs=self.EPOCHS,
+            batch_size=self.BATCH_SIZE,
+            validation_split=self.VALIDATION_SPLIT
+        )
+
+        # Evaluate default model
+        default_accuracy, default_metric = evaluate_model(
+            default_model, X_train, y_train, fairness_metric
+        )
+
+        return fauci_accuracy, fauci_metric, default_accuracy, default_metric
 
     def testFauciOneSensitiveAttrSPD(self):
-        X_train = self.X.copy()
-        y_train = self.y.copy()
-        X_train["income"] = y_train
-        fauci_train_dataset = fl.DataFrame(X_train)
-        fauci_train_dataset.targets = "income"
-        fauci_train_dataset.sensitive = "sex"
-        fauci_model = create_model()
-        X_train.drop(columns=["income"], inplace=True)
-
-        fauciModel = Fauci(
-            fauci_model,
-            loss="binary_crossentropy",
-            regularizer="sp",
-            optimizer="adam",
-            metrics=["accuracy"],
-        )
-        fauciModel.fit(
-            fauci_train_dataset,
-            converting_to_type=float,
-            epochs=self.EPOCHS,
-            batch_size=32,
-            validation_split=0.2,
-        )
-        fauci_accuracy, fauci_spd = evaluate_model(fauciModel, X_train, y_train, "spd")
-
-        X_train = self.X.copy()
-        y_train = self.y.copy()
-        X_train = X_train.astype(float)
-        y_train = y_train.astype(float)
-        default_model = create_model()
-        default_model.compile(
-            loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
-        )
-        default_model.fit(
-            X_train, y_train, epochs=self.EPOCHS, batch_size=32, validation_split=0.2
-        )
-        default_model_accuracy, default_model_spd = evaluate_model(
-            default_model, X_train, y_train, "spd"
+        # Test Fauci using Statistical Parity Difference (SPD)
+        fauci_accuracy, fauci_spd, default_accuracy, default_spd = self.train_and_evaluate(
+            regularizer="sp", fairness_metric="spd"
         )
 
-        print("FAUCI: accuracy: ", fauci_accuracy, " spd: ", fauci_spd)
-        print(
-            "Default Model: accuracy: ",
-            default_model_accuracy,
-            " spd: ",
-            default_model_spd,
-        )
+        # Print results
+        print("FAUCI: accuracy:", fauci_accuracy, "spd:", fauci_spd)
+        print("Default Model: accuracy:", default_accuracy, "spd:", default_spd)
 
+        # Assert fairness improvement
         assert (
-            fauci_spd[{"income": 1, "sex": 1}]
-            <= default_model_spd[{"income": 1, "sex": 1}]
-        ), f"Expected {fauci_spd}, to be less than {default_model_spd}"
+                fauci_spd[{"income": 1, "sex": 1}]
+                <= default_spd[{"income": 1, "sex": 1}]
+        ), f"Expected {fauci_spd}, to be less than {default_spd}"
 
     def testFauciOneSensitiveAttrDI(self):
-        X_train = self.X.copy()
-        y_train = self.y.copy()
-        X_train["income"] = y_train
-        fauci_train_dataset = fl.DataFrame(X_train)
-        fauci_train_dataset.targets = "income"
-        fauci_train_dataset.sensitive = "sex"
-        fauci_model = create_model()
-        X_train.drop(columns=["income"], inplace=True)
-
-        fauciModel = Fauci(
-            fauci_model,
-            loss="binary_crossentropy",
-            regularizer="di",
-            optimizer="adam",
-            metrics=["accuracy"],
-        )
-        fauciModel.fit(
-            fauci_train_dataset,
-            converting_to_type=float,
-            epochs=self.EPOCHS,
-            batch_size=32,
-            validation_split=0.2,
-        )
-        fauci_accuracy, fauci_di = evaluate_model(fauciModel, X_train, y_train, "di")
-
-        X_train = self.X.copy()
-        y_train = self.y.copy()
-        X_train = X_train.astype(float)
-        y_train = y_train.astype(float)
-        default_model = create_model()
-        default_model.compile(
-            loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
-        )
-        default_model.fit(
-            X_train, y_train, epochs=self.EPOCHS, batch_size=32, validation_split=0.2
-        )
-        default_model_accuracy, default_model_di = evaluate_model(
-            default_model, X_train, y_train, "di"
+        # Test Fauci using Disparate Impact (DI)
+        fauci_accuracy, fauci_di, default_accuracy, default_di = self.train_and_evaluate(
+            regularizer="di", fairness_metric="di"
         )
 
-        print("FAUCI: accuracy: ", fauci_accuracy, " di: ", fauci_di)
-        print(
-            "Default Model: accuracy: ",
-            default_model_accuracy,
-            " di: ",
-            default_model_di,
-        )
+        # Print results
+        print("FAUCI: accuracy:", fauci_accuracy, "di:", fauci_di)
+        print("Default Model: accuracy:", default_accuracy, "di:", default_di)
 
+        # Assert fairness improvement
         fauci_distance = abs(fauci_di[{"income": 1, "sex": 1}] - 1)
-        model_distance = abs(default_model_di[{"income": 1, "sex": 1}] - 1)
-
+        model_distance = abs(default_di[{"income": 1, "sex": 1}] - 1)
         assert (
-            fauci_distance <= model_distance
+                fauci_distance <= model_distance
         ), f"Expected {fauci_distance}, to be less than {model_distance}"
 
 
